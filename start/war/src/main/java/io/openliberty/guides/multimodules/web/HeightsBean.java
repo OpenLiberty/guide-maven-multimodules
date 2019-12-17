3d20651dc75eff1f1404962d7f6ae238d544c532
evelinec <evelinec@ca.ibm.com>
2019-12-17 14:46:32 -0500

    Formatted start directory

--- start/war/src/main/java/io/openliberty/guides/multimodules/web/HeightsBean.java
+++ start/war/src/main/java/io/openliberty/guides/multimodules/web/HeightsBean.java
@@ -13,51 +13,52 @@
 package io.openliberty.guides.multimodules.web;
 
 public class HeightsBean implements java.io.Serializable {
-  private String heightCm = null;
-  private String heightFeet = null;
-  private String heightInches = null;
-  private int cm = 0;
-  private int feet = 0;
-  private int inches = 0;
+    private String heightCm = null;
+    private String heightFeet = null;
+    private String heightInches = null;
+    private int cm = 0;
+    private int feet = 0;
+    private int inches = 0;
 
-  public HeightsBean() {
-  }
+    public HeightsBean() {
+    }
 
-  // Capitalize the first letter of the name i.e. first letter after get
-  // If first letter is not capitalized, it must match the property name in index.jsp
-  public String getHeightCm() {
-    return heightCm;
-  }
+    // Capitalize the first letter of the name i.e. first letter after get
+    // If first letter is not capitalized, it must match the property name in
+    // index.jsp
+    public String getHeightCm() {
+        return heightCm;
+    }
 
-  public String getHeightFeet() {
-    return heightFeet;
-  }
+    public String getHeightFeet() {
+        return heightFeet;
+    }
 
-  public String getHeightInches() {
-    return heightInches;
-  }
+    public String getHeightInches() {
+        return heightInches;
+    }
 
-  public void setHeightCm(String heightcm) {
-    this.heightCm = heightcm;
-  }
+    public void setHeightCm(String heightcm) {
+        this.heightCm = heightcm;
+    }
 
-  // Need an input as placeholder, you can choose not to use the input
-  public void setHeightFeet(String heightfeet) {
-    this.cm = Integer.valueOf(heightCm);
-    // tag::dependency-code1[]
-    // TO-DO: ADD THE getFeet CODE SNIPPET HERE
-    // end::dependency-code1[]
-    String result = String.valueOf(feet);
-    this.heightFeet = result;
-  }
+    // Need an input as placeholder, you can choose not to use the input
+    public void setHeightFeet(String heightfeet) {
+        this.cm = Integer.valueOf(heightCm);
+        // tag::dependency-code1[]
+        // TO-DO: ADD THE getFeet CODE SNIPPET HERE
+        // end::dependency-code1[]
+        String result = String.valueOf(feet);
+        this.heightFeet = result;
+    }
 
-  public void setHeightInches(String heightinches) {
-    this.cm = Integer.valueOf(heightCm);
-    // tag::dependency-code2[]
-    // TO-DO: ADD THE getInches CODE SNIPPET HERE
-    // end::dependency-code2[]
-    String result = String.valueOf(inches);
-    this.heightInches = result;
-  }
+    public void setHeightInches(String heightinches) {
+        this.cm = Integer.valueOf(heightCm);
+        // tag::dependency-code2[]
+        // TO-DO: ADD THE getInches CODE SNIPPET HERE
+        // end::dependency-code2[]
+        String result = String.valueOf(inches);
+        this.heightInches = result;
+    }
 
 }
